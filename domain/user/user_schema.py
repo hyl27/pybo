4b5957c86f96f9813d88f26ae858d3f2a0dc2faf
hyl27 <100427hyl@gmail.com>
2024-02-07 16:49:46 +0900

    pw

--- domain/user/user_schema.py
+++ domain/user/user_schema.py
@@ -13,6 +13,19 @@ class UserCreate(BaseModel):
             raise ValueError('필수 입력값 입니다.')
         return v
 
+    @field_validator('password1')
+    def check_password(cls, value):
+        value = str(value)
+        if len(value) < 8:
+            raise ValueError("8글자 이상 입력하세요.")
+        if not any(c.isupper() for c in value):
+            raise ValueError("대문자 하나 이상 포함하세요.")
+        if not any(c.islower() for c in value):
+            raise ValueError("소문자 하나 이상 포함하세요.")
+        if not any(c.isdigit() for c in value):
+            raise ValueError("숫자 하나 이상 포함하세요.")
+        return value
+
     @field_validator('password2')
     def passwords_match(cls, v, info: FieldValidationInfo):
         if 'password1' in info.data and v != info.data['password1']:
